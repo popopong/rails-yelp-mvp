501209b621c1961b6c425948e93b08e6a55ee93d
Poyan Ng <poyanng.npy@gmail.com>
2020-05-13 16:37:28 -0400

    nice

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,5 @@ class Restaurant < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :phone_number, presence: true
   validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "belgian" }
+imperial = { name: "Imperial", address: "St Catherine", category: "chinese" }
+tamjai = { name: "Tam Jai", address: "2A Nathan Road, Tsim Sha Tsui", category: "chinese" }
+carol = { name: "Carol Bistro", address: "Miles End", category: "italian" }
+
+[dishoom, pizza_east, imperial, tamjai, carol].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
